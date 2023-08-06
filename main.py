768e2371d15d9e866ca225b5fdd7e7e1b03add13
sevase <kotaseva8@gmail.com>
2023-08-07 01:05:37 +0300

    TIK-001: сделал процесс передачи в данных между функциями

--- main.py
+++ main.py
@@ -1,45 +1,120 @@
-from rich.console import Console
-from rich.panel import Panel
+# класс обработки файла
+class TxtFileRefactor:
+    def __init__(self, name_file):
+        self.name_file = name_file
 
-def del_words():
-    with open('textWAR_I.txt', 'r', encoding='utf-8') as file_text_read:
-        new_file = [wrd for wrd in file_text_read if 'смех' not in wrd]
-        return new_word_file(new_file)
+    # обработование файла на удаленние слов
+    def del_words(self):
+        with open(self.name_file, 'r', encoding='utf-8') as file_text_read:
+            new_file = [wrd for wrd in file_text_read if 'смех' not in wrd]
+            return new_file
 
-def new_word_file(new_file):
-    with open('newWORD.txt', 'w', encoding='utf-8') as file_text_write:
-        file_text_write.writelines(new_file)
 
+# класс по созданию нового файла
+class NewTxtFile(TxtFileRefactor):
+    # создание нового правильного файла
+    def new_word_file(self, new_file):
+        with open('newWORD.txt', 'w', encoding='utf-8') as file_text_write:
+            file_text_write.writelines(new_file)
+
+    def process_file(self):
+        new_file = self.del_words()
+        self.new_word_file(new_file)
+
+
+# класс для дизайна консоли
+class Design:
+    from rich.console import Console
+    from rich.panel import Panel
+
+    def __init__(self, answer_options=None):
+        self.__answer_options = answer_options
+
+
+    @property
+    def get_answer_options(self):
+        return self.__answer_options
+
+    @get_answer_options.setter
+    def set_answer_options(self, value):
+        if value in ('1', '2'):
+            self.__answer_options = value
+        else:
+            self.__answer_options = 'another'
+
+    # дизайн главной панели
+    @staticmethod
+    def osn_panel_menu():
+        console = Design.Console()
+
+        menu = Design.Panel.fit(
+            "[bold]Выберите опцию:[/bold]\n"
+            "1. Удалить определенные слова\n"
+            "2. Удалить слова с определенной буквой\n",
+            border_style="yellow",
+            padding=(1, 2),
+        )
+        console.print(menu)
+        print()
+
+    # панель ввода номера опции
+    @staticmethod
+    def input_new_options():
+        console = Design.Console()
+
+        menu = Design.Panel.fit(
+            "Введите номер выбранной опции"
+        )
+        console.print(menu)
+
+    # дизайн ответа выбранной опции
+    def choice_options_answer(self):
+        console = Design.Console()
+
+        if self.__answer_options == '1':
+            menu = Design.Panel.fit(
+                    "Вы выбрали Первую опцию!"
+                )
+        elif self.__answer_options == '2':
+            menu = Design.Panel.fit(
+                "Вы выбрали Вторую опцию!"
+            )
+        else:
+            menu = Design.Panel.fit(
+                    "Неверный выбор!"
+                )
+
+        console.print(menu)
 
 
 def main():
-    console = Console()
+    Design.osn_panel_menu()
+    Design.input_new_options()
 
-    menu = Panel.fit(
-        "[bold]Выберите опцию:[/bold]\n"
-        "1. Первая опция\n"
-        "2. Вторая опция\n",
-        border_style="yellow",
-        padding=(1, 2),
-    )
 
-    console.print(menu)
+    # выбор опции
+    choice = input('- ')
+    design = Design()
+    design.set_answer_options = choice
 
-    choice = input("Введите номер выбранной опции: ")
-
-    if choice == "1":
-        print("Вы выбрали Первую опцию!")
-    elif choice == "2":
-        print("Вы выбрали Вторую опцию!")
+    if design.get_answer_options == '1':
+        design.choice_options_answer()
+        t = NewTxtFile('textWAR_I.txt')
+        t.process_file()
+    elif design.get_answer_options == '2':
+        design.choice_options_answer()
     else:
-        print("Неверный выбор!")
+        design.choice_options_answer()
 
-    del_words()
+
+
+
 
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
 
 
 
