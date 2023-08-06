ea1017e578dfffebc4f226438eebcf1409390c88
sevase <kotaseva8@gmail.com>
2023-08-06 17:25:29 +0300

    TIK-001: сделана заготовочка эксперементальная под обработку файла

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+def del_words():
+    with open('textWAR_I.txt', 'r', encoding='utf-8') as file_text_read:
+        new_file = [wrd for wrd in file_text_read if 'смех' not in wrd]
+        return new_word_file(new_file)
+
+def new_word_file(new_file):
+    with open('newWORD.txt', 'w', encoding='utf-8') as file_text_write:
+        file_text_write.writelines(new_file)
 
 
 
+def main():
+    del_words()
 
 
 
-
-
+if __name__ == '__main__':
+    main()
 
 
 
